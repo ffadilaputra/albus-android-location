67f4e54229d97f87916b5d35c8c194538f4a593c
ffadilaputra <i.fadilaputra@gmail.com>
2018-11-13 08:50:16 +0700

    Dah kelar 😌

---
+++ app/src/main/java/geolocation/ffadilaputra/org/modul_geolocation/AddressAsycTask.java
@@ -0,0 +1,77 @@
+package geolocation.ffadilaputra.org.modul_geolocation;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class AddressAsycTask extends AsyncTask<Location , Void, String> {
+
+	interface onTaskRampung{
+		void onTaskCompleted(String result);
+	}
+
+	private Context mContext;
+	private onTaskRampung mListenere;
+
+	public AddressAsycTask(Context context, onTaskRampung rampung) {
+		mContext = context;
+		mListenere = rampung;
+	}
+
+	@Override
+	protected void onPostExecute(String s) {
+		mListenere.onTaskCompleted(s);
+		super.onPostExecute(s);
+	}
+
+	@Override
+	protected String doInBackground(Location... locations) {
+		Geocoder geocoder = new Geocoder(mContext,Locale.getDefault());
+		Location location = locations[0];
+		String resultMessage = "";
+		List<Address> addresses = null;
+
+
+		try {
+			addresses = geocoder.getFromLocation(
+					location.getLatitude(),
+					location.getLongitude(),1
+			);
+
+			if (addresses == null || addresses.size() == 0){
+				if (resultMessage.isEmpty()){
+					resultMessage = "ALamat tida ditemukan";
+					Log.e("Lokasi Error bto",resultMessage);
+				}
+			}else{
+				Address address = addresses.get(0);
+				ArrayList<String> listAddress = new ArrayList<>();
+
+				for (int i=0; i<= address.getMaxAddressLineIndex(); i++){
+					listAddress.add(address.getAddressLine(i));
+				}
+
+				resultMessage = TextUtils.join("\n", listAddress);
+
+			}
+
+		}catch (IOException e){
+			resultMessage = "Layanan tida tersedia";
+			Log.e("Lokasi eror brader",resultMessage,e);
+		}catch (IllegalArgumentException ilegal){
+			resultMessage = "Koordinat invalid";
+			Log.e("Lokasi Error",resultMessage+"."+"Lattitide"+location.getLatitude()+"Logitude"+location.getLongitude(),ilegal);
+		}
+
+		return resultMessage;
+	}
+}
--- app/src/main/java/geolocation/ffadilaputra/org/modul_geolocation/MainActivity.java
+++ app/src/main/java/geolocation/ffadilaputra/org/modul_geolocation/MainActivity.java
@@ -1,6 +1,8 @@
 package geolocation.ffadilaputra.org.modul_geolocation;
 
 import android.Manifest;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -10,20 +12,28 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AddressAsycTask.onTaskRampung{
 
     private Button btn;
     private Location lastLocation;
     private TextView txtLastLocation;
     private FusedLocationProviderClient mFusedLocationClient;
+	private AnimatorSet rotateAnime;
+	private ImageView imageView;
 
+	private boolean mtrackingLocation;
+	private LocationCallback mLocationCallback;
 
 	private static final int REQUEST_LOCATION_PERMISSION = 1;
 
@@ -32,37 +42,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView)findViewById(R.id.image_view);
         txtLastLocation = (TextView)findViewById(R.id.last_location);
         btn = (Button) findViewById(R.id.btn_submit);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+		rotateAnime = (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.rotate);
+		rotateAnime.setTarget(imageView);
+
+		mLocationCallback = new LocationCallback(){
+			@Override
+			public void onLocationResult(LocationResult locationResult) {
+				if (mtrackingLocation){
+					new AddressAsycTask(MainActivity.this,MainActivity.this).execute(locationResult.getLastLocation());
+				}
+			}
+		};
+
         btn.setOnClickListener(new View.OnClickListener(){
 			@Override
             public void onClick(View v) {
-                getLocation();
+
+                if (!mtrackingLocation){
+					getLocation();
+				}else{
+                	stopLocation();
+				}
             }
         });
 
     }
 
-    private void getLocation() {
+	private void stopLocation() {
+    	if (mtrackingLocation){
+    		mtrackingLocation = false;
+    		mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    		btn.setText("Mulai tracking gan");
+    		txtLastLocation.setText("Tracking dihentikan ");
+    		rotateAnime.end();
+		}
+	}
+
+	private void getLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 				ActivityCompat.requestPermissions(this,new String[]{
 						Manifest.permission.ACCESS_FINE_LOCATION
 				},REQUEST_LOCATION_PERMISSION);
         }else{
             //Log.d("GetPermissions","getLocation: permission granted");
-			mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-				@Override
-				public void onSuccess(Location location) {
-					if (location != null){
-						lastLocation = location;
-						txtLastLocation.setText(getString(R.string.location_text,lastLocation.getLatitude(),lastLocation.getLongitude(),lastLocation.getTime()));
-					}else{
-						txtLastLocation.setText("Lokasi tidak tersedia");
-					}
-				}
-			});
+//			mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+//				@Override
+//				public void onSuccess(Location location) {
+//					if (location != null){
+////						lastLocation = location;
+////						txtLastLocation.setText(getString(R.string.location_text,lastLocation.getLatitude(),lastLocation.getLongitude(),lastLocation.getTime()));
+//						new AddressAsycTask(MainActivity.this,MainActivity.this).execute(location);
+//					}else{
+//						txtLastLocation.setText("Lokasi tidak tersedia");
+//					}
+//				}
+//			});
+			mFusedLocationClient.requestLocationUpdates(getLocationRequest(),mLocationCallback,null);
+			txtLastLocation.setText(getString(R.string.alamat_text,"Sedang mencari jodoh gan", System.currentTimeMillis()));
+			mtrackingLocation = true;
+			btn.setText("Berhenti dong gan");
+			rotateAnime.start();
         }
+
     }
 
 	@Override
@@ -76,4 +121,21 @@ public class MainActivity extends AppCompatActivity {
 				}
 		}
 	}
+
+	@Override
+	public void onTaskCompleted(String result) {
+    	if (mtrackingLocation){
+			txtLastLocation.setText(getString(R.string.alamat_text, result, System.currentTimeMillis()));
+		}
+
+	}
+
+	private LocationRequest getLocationRequest(){
+    	LocationRequest locationRequest = new LocationRequest();
+    	locationRequest.setInterval(10000);
+    	locationRequest.setFastestInterval(5000);
+    	locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    	return locationRequest;
+	}
+
 }
