8b13ac65508fe3db8379bbd3cb6ba46b0af94fb9
ffadilaputra <i.fadilaputra@gmail.com>
2018-11-20 19:38:50 +0700

    Pake state uhuyy 🧙🏻‍♂️

--- README.md
+++ README.md
@@ -1,78 +1,10 @@
 # 📝 Android Praktikum 09 - Location
 
 
-## 🎺 Hasil
-!['hehehe'](./gif/TIO6_15.gif)
+## 🎺 Hasil Location lanjutan
+!['hehehe'](https://media.giphy.com/media/C8uurV5Rb8Al9Pqfh1/giphy.gif)
 
 
-## Praktikum 9.2
-
-```java
-mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-	@Override
-		public void onSuccess(Location location) {
-			if (location != null){
-				lastLocation = location;
-				txtLastLocation.setText(getString(R.string.location_text,lastLocation.getLatitude(),lastLocation.getLongitude(),lastLocation.getTime()));
-				new AddressAsycTask(MainActivity.this,MainActivity.this).execute(location);
-			}else{
-				txtLastLocation.setText("Lokasi tidak tersedia");
-			}
-		}
-	});
-```
-
-## Menyiapkan beberapa variabel
-
-```java
-	Geocoder geocoder = new Geocod (mContext,Locale.getDefault());
-	Location location = locations[0];
-	String resultMessage = "";
-	List<Address> addresses = null;
-```
-
-Kemudian melakukan try catch untuk menghandle sebuah exception
-
-```java
-try{
-
-}catch (IOException e){
-}catch (IllegalArgumentException ilegal){
-
-}
-
-```
-
-Geolocation to lattitude logitude
-
-```java
-addresses = geocoder.getFromLocation(
-	location.getLatitude(),
-	location.getLongitude(),1);
-```
-
-Kemudian simpan dalam bentuk array
-```java
-Address address = addresses.get(0);
-ArrayList<String> listAddress = new ArrayList<>();
-
-for (int i=0; i<= address.getMaxAddressLineIndex(); i++){
-	listAddress.add(address.getAddressList(i));
-}
-
-    resultMessage = TextUtils.join("\n", listAddress);
-```
-
-### Praktikum 9.4
-
-Menampilkan data secara periodik
-
-```java
-mFusedLocationClient.requestLocationUpdates(getLocationRequest(),mLocationCallback,null);
-```
 
 ## 🌝 Penyusun
 - Ivan Fadila Putra - TO06_15
-
-## 📦 File Pendukung
-- [Sample GPX File](./gif/run.gpx)
--- app/src/main/java/geolocation/ffadilaputra/org/modul_geolocation/MainActivity.java
+++ app/src/main/java/geolocation/ffadilaputra/org/modul_geolocation/MainActivity.java
@@ -3,10 +3,12 @@ package geolocation.ffadilaputra.org.modul_geolocation;
 import android.Manifest;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +19,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -27,6 +31,7 @@ import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.PlaceLikelihood;
 import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -34,7 +39,8 @@ import com.google.android.gms.tasks.Task;
 public class MainActivity extends AppCompatActivity implements AddressAsycTask.onTaskRampung{
 
 	private static final int REQUEST_LOCATION_PERMISSION = 1;
-    private Button btn;
+	private static final int REQUEST_PICK_PLACE = 1;
+    private Button btn,btn_pick;
     private Location lastLocation;
     private TextView txtLastLocation;
     private FusedLocationProviderClient mFusedLocationClient;
@@ -45,7 +51,47 @@ public class MainActivity extends AppCompatActivity implements AddressAsycTask.o
 	private PlaceDetectionClient mPlaceDetectionClient;
 	private String mLastPlaceName;
 
-    @Override
+	private String NAME = "";
+	private String ADDRESS = "";
+	private static int IMAGE = -1;
+
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_OK){
+			Place place = PlacePicker.getPlace(this,data);
+			setLocationType(place);
+			txtLastLocation.setText(getString(R.string.alamat_text,place.getName(),place.getAddress(),System.currentTimeMillis()));
+			NAME = place.getName().toString();
+			ADDRESS = place.getName().toString();
+			IMAGE = setLocationType(place);
+			imageView.setImageResource(IMAGE);
+		}else{
+			txtLastLocation.setText("Hmmmm pilih dulu dong");
+		}
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		if (savedInstanceState.getString("placeName") == ""){
+			txtLastLocation.setText("Pijet tombol untuk dapatkan lokasi anda");
+		}else{
+			txtLastLocation.setText(getString(R.string.alamat_text,savedInstanceState.getString("placeName"),savedInstanceState.getString("placeAddress"),System.currentTimeMillis()));
+			imageView.setImageResource(savedInstanceState.getInt("placeImage"));
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString("placeName", NAME);
+		outState.putString("placeAddress", ADDRESS);
+		outState.putInt("placeImage", IMAGE);
+	}
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -54,6 +100,7 @@ public class MainActivity extends AppCompatActivity implements AddressAsycTask.o
         imageView = (ImageView)findViewById(R.id.image_view);
         txtLastLocation = (TextView)findViewById(R.id.last_location);
         btn = (Button) findViewById(R.id.btn_submit);
+        btn_pick = (Button)findViewById(R.id.btn_pick);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 		rotateAnime = (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.rotate);
 		rotateAnime.setTarget(imageView);
@@ -66,6 +113,21 @@ public class MainActivity extends AppCompatActivity implements AddressAsycTask.o
 				}
 			}
 		};
+
+		btn_pick.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+				try {
+					startActivityForResult(builder.build(MainActivity.this),REQUEST_PICK_PLACE);
+				}catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e ){
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+
 
         btn.setOnClickListener(new View.OnClickListener(){
 			@Override
@@ -148,9 +210,12 @@ public class MainActivity extends AppCompatActivity implements AddressAsycTask.o
 									maxLikelihood = placeLikelihood.getLikelihood();
 									currentPlace = placeLikelihood.getPlace();
 									if (currentPlace != null){
-
 										setLocationType(currentPlace);
 										txtLastLocation.setText(getString(R.string.alamat_text,currentPlace.getName(),result,System.currentTimeMillis()));
+										NAME = placeLikelihood.getPlace().getName().toString();
+										ADDRESS = placeLikelihood.getPlace().getAddress().toString();
+										IMAGE = setLocationType(placeLikelihood.getPlace());
+										imageView.setImageResource(IMAGE);
 									}else{
 										txtLastLocation.setText(getString(R.string.alamat_text,"Nama Lokasi tidak ditemukan gan",result,System.currentTimeMillis()));
 									}
@@ -173,7 +238,7 @@ public class MainActivity extends AppCompatActivity implements AddressAsycTask.o
     	return locationRequest;
 	}
 
-	private void setLocationType(Place currentPlace){
+	private int setLocationType(Place currentPlace){
     	int drawableID = -1;
 
     	for (Integer placeType:currentPlace.getPlaceTypes()){
@@ -190,13 +255,22 @@ public class MainActivity extends AppCompatActivity implements AddressAsycTask.o
 				case Place.TYPE_MOVIE_THEATER:
 					drawableID = R.mipmap.ic_teather;
 					break;
+				case Place.TYPE_BUS_STATION:
+					drawableID = R.mipmap.ic_ngeng;
+					break;
+				case Place.TYPE_HOSPITAL:
+					drawableID = R.mipmap.ic_rs;
+					break;
+				case Place.TYPE_ATM:
+					drawableID = R.mipmap.ic_atm;
+					break;
 			}
 		}
 		if (drawableID < 0){
     		drawableID = R.mipmap.ic_x_round;
 		}
 		imageView.setImageResource(drawableID);
-		//return drawableID;
+		return drawableID;
 	}
 
 }
